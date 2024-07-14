2c79a7eb6538c6507d771f9885e64a425f2c5ec5
James Harris <jameseharrisii@gmail.com>
2024-07-13 17:28:05 -0700

    Understanding global scope

--- app1.js
+++ app1.js
@@ -6,11 +6,16 @@ function myFun1() {
 }
 
 function myFun2(message,name,val) {
-    console.log(message,name,val)
+    console.log('1.', message,name,val)
     const val1 = name + ' ' + message; 
-    console.log(val1)
+    console.log('2.', val1)
+    return val1
 }
 // Invoke || call the function
 myFun1()
 myFun2('hello','James',2000)
 myFun2('Hello', 'funk_Nose', 90)
+
+
+const val1 = myFun2('Welcome','James',8000);
+console.log(val1)
